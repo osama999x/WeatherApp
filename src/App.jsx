75e6932987769b716c1d5bed42548712aa9eb284
osama999x <osamakhan99@hotmail.com>
2025-02-25 17:46:08 +0500

    Initial commit - Added Weather App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import './App.css'
+import Weather from './components/Weather'
+
+function App() {
+    // const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <Weather />
+        </>
+    )
+}
+
+export default App
